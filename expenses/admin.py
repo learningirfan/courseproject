5ba7aca2f62b885cca5f1c85937e74c288cc849e
learningirfan <learningirfan@gmail.com>
2024-04-18 14:43:34 -0600

    Register the model Exps in admin.py

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from expenses.models import Exps
 
 # Register your models here.
+class ExpsAdmin(admin.ModelAdmin):
+    """ Admin Interface for Exps """
+
+admin.site.register(Exps)
