d153e5c1416465a4c41b85ef10a2351490d921f2
learningirfan <learningirfan@gmail.com>
2024-04-18 19:44:23 -0600

    added form to create new expense record, successfully saved data to database and retrieved for viewing

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,10 +1,11 @@
-from .views import MainView
+from .views import MainView, AddNew, ExpEdit
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path("", MainView.as_view(), name="Main")
-
-
-
+    path("", MainView.as_view(), name="Main"),
+    path("create/", AddNew.as_view(), name="create"),
+    path("<pk>/edit", ExpEdit.as_view(), name="edit"),
 
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Exps
 from .forms import ExpsForm
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
 
 # Create your views here.
 class MainView(ListView):
@@ -10,3 +11,17 @@ class MainView(ListView):
 
     def get_queryset(self):
         return Exps.objects.all()
+    
+
+class AddNew(CreateView):
+    model = Exps
+    form_class = ExpsForm
+    template_name = "create.html"
+    success_url = reverse_lazy("Main")
+
+class ExpEdit(UpdateView):
+    model = Exps
+    fields =["date", "amt", "cat", "ven", "cap", "pmeth"]
+    success_url = reverse_lazy("Main")
+    
+
