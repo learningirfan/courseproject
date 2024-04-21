18d895657701a83752b1276f559554fbe83cc391
learningirfan <learningirfan@gmail.com>
2024-04-21 16:22:31 -0600

    added datepicker to create and edit form

--- expenses/forms.py
+++ expenses/forms.py
@@ -2,7 +2,12 @@ from django import forms
 from django.forms import ModelForm, DateField
 from .models import Exps
 
+
 class ExpsForm(forms.ModelForm):
     class Meta:
         model = Exps
         fields =["date", "amt", "cat", "ven", "cap", "pmeth"]
+        widgets = {
+            'date': forms.DateInput(attrs={'class': 'datepicker'}),
+
+        }
