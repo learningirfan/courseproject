bb831a8b0185e244dbb4a1061fc6d204ad6d82c5
learningirfan <learningirfan@gmail.com>
2024-04-22 11:16:08 -0600

    update ALLOWED-HOSTS inb settings.py

--- ExpProject/settings.py
+++ ExpProject/settings.py
@@ -133,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'expenses/static')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
